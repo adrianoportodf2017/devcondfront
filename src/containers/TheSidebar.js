65ee99fc43dcb77ee912b42d9a9b8d21ac26f14e
adriano <adrianobr00@gmail.com>
2022-09-02 17:52:27 -0300

    atualizando sidebar

--- src/containers/TheSidebar.js
+++ src/containers/TheSidebar.js
@@ -3,16 +3,12 @@ import { useSelector, useDispatch } from 'react-redux'
 import {
   CCreateElement,
   CSidebar,
-  CSidebarBrand,
   CSidebarNav,
-  CSidebarNavDivider,
   CSidebarNavTitle,
   CSidebarMinimizer,
-  CSidebarNavDropdown,
   CSidebarNavItem,
 } from '@coreui/react'
 
-import CIcon from '@coreui/icons-react'
 
 // sidebar nav config
 import navigation from './_nav'
@@ -26,25 +22,13 @@ const TheSidebar = () => {
       show={show}
       onShowChange={(val) => dispatch({type: 'set', sidebarShow: val })}
     >
-      <CSidebarBrand className="d-md-down-none" to="/">
-        <CIcon
-          className="c-sidebar-brand-full"
-          name="logo-negative"
-          height={35}
-        />
-        <CIcon
-          className="c-sidebar-brand-minimized"
-          name="sygnet"
-          height={35}
-        />
-      </CSidebarBrand>
+    
       <CSidebarNav>
+        <img src="/homelogo.png" className="mt-2 mb-3 ml-auto mr-auto" width="70%" />
 
         <CCreateElement
           items={navigation}
           components={{
-            CSidebarNavDivider,
-            CSidebarNavDropdown,
             CSidebarNavItem,
             CSidebarNavTitle
           }}
