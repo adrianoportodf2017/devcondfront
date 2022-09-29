4e0fdb75748c7b8841060e89c6d78232cb807246
adriano <adrianobr00@gmail.com>
2022-09-29 18:05:55 -0300

    criando função add em avisos

--- src/services/api.js
+++ src/services/api.js
@@ -56,6 +56,11 @@ export default () => {
       let json = await request('get', '/walls', {}, token);
       return json;
     },
+    addWall: async  (data) => {
+      let token = localStorage.getItem('token');
+      let json = await request('post', '/walls', data, token);
+      return json;
+    },
     updateWall: async (id, data) => {
       let token = localStorage.getItem('token');
       let json = await request('put', `/wall/${id}`, data, token);
