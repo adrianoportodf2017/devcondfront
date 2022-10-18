950c530a04e6a5e2472599a064f2d241453b419a
adriano <adrianobr00@gmail.com>
2022-10-18 17:32:27 -0300

    criando crud documentos

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-//const baseUrl = 'https://devcondbackend.agenciatecnet.com.br/public/api/admin'
-const baseUrl = 'http://localhost:8000/api/admin'
+const baseUrl = 'https://devcondbackend.agenciatecnet.com.br/public/api/admin'
+//const baseUrl = 'http://localhost:8000/api/admin'
 //const baseUrl = 'https://api.b7web.com.br/devcond/api/admin'
 
 const request = async (method, endpoint, params, token = null) => {
@@ -31,9 +31,9 @@ const request = async (method, endpoint, params, token = null) => {
 
 export default () => {
   return {
-     /********************************************************************************/ 
- /******************************--__-- Login --__--***********************************/ 
-   /********************************************************************************/ 
+    /********************************************************************************/
+    /******************************--__-- Login --__--***********************************/
+    /********************************************************************************/
     getToken: () => {
       return localStorage.getItem('token');
     },
@@ -45,7 +45,7 @@ export default () => {
     },
 
     login: async (email, password) => {
-      let json = await request('post', '/auth/login', {email, password});
+      let json = await request('post', '/auth/login', { email, password });
       return json
     },
 
@@ -55,17 +55,17 @@ export default () => {
       localStorage.removeItem('token');
       return json
     },
-  
-   /********************************************************************************/ 
- /******************************--__-- Walls --__--***********************************/ 
-   /********************************************************************************/ 
+
+    /********************************************************************************/
+    /******************************--__-- Walls --__--***********************************/
+    /********************************************************************************/
 
     getWall: async () => {
       let token = localStorage.getItem('token');
       let json = await request('get', '/walls', {}, token);
       return json;
     },
-    addWall: async  (data) => {
+    addWall: async (data) => {
       let token = localStorage.getItem('token');
       let json = await request('post', '/walls', data, token);
       return json;
@@ -77,33 +77,59 @@ export default () => {
     },
     removeWall: async (id) => {
       let token = localStorage.getItem('token');
-      let json = await request('delete', `/wall/${id}`, { }, token);
+      let json = await request('delete', `/wall/${id}`, {}, token);
       return json;
     },
 
-    /********************************************************************************/ 
- /******************************--__-- Documents --__--***********************************/ 
-   /********************************************************************************/ 
+    /********************************************************************************/
+    /******************************--__-- Documents --__--***********************************/
+    /********************************************************************************/
 
-   getDocuments: async () => {
-    let token = localStorage.getItem('token');
-    let json = await request('get', '/docs', {}, token);
-    return json;
-  },
-  addWall: async  (data) => {
-    let token = localStorage.getItem('token');
-    let json = await request('post', '/walls', data, token);
-    return json;
-  },
-  updateWall: async (id, data) => {
-    let token = localStorage.getItem('token');
-    let json = await request('put', `/wall/${id}`, data, token);
-    return json;
-  },
-  removeWall: async (id) => {
-    let token = localStorage.getItem('token');
-    let json = await request('delete', `/wall/${id}`, { }, token);
-    return json;
-  },
+    getDocuments: async () => {
+      let token = localStorage.getItem('token');
+      let json = await request('get', '/docs', {}, token);
+      return json;
+    },
+    addDocument: async (data) => {
+      let token = localStorage.getItem('token');
+      let formData = new FormData();
+      formData.append('title', data.title);
+      if (data.file) {
+        formData.append('file', data.file);
+      }
+      let req = await fetch(`${baseUrl}/docs`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+        body: formData
+      });
+
+      let json = await req.json();
+      return json;
+    },
+    updateDocument: async (id, data) => {
+      let token = localStorage.getItem('token');
+      let formData = new FormData();
+      formData.append('title', data.title);
+      if (data.file) {
+        formData.append('file', data.file);
+      }
+      let req = await fetch(`${baseUrl}/doc/${id}`, {
+        method: 'POST',
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+        body: formData
+      });
+
+      let json = await req.json();
+      return json;
+    },
+    removeDocument: async (id) => {
+      let token = localStorage.getItem('token');
+      let json = await request('delete', `/doc/${id}`, {}, token);
+      return json;
+    },
   }
 }
--- src/views/Documents.js
+++ src/views/Documents.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import {
-    CButton,
-    CButtonGroup,
-    CCard,
-    CCardBody,
-    CCardHeader,
-    CCol,
-    CDataTable,
-    CRow,
-    CModal,
-    CModalHeader,
-    CModalBody,
-    CModalFooter ,
-    CFormGroup,
-    CLabel,
-    CTextarea,
-    CInput
+  CButton,
+  CButtonGroup,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow,
+  CModal,
+  CModalHeader,
+  CModalBody,
+  CModalFooter,
+  CFormGroup,
+  CLabel,
+  CTextarea,
+  CInput,
+  CInputFile
 } from '@coreui/react';
 import CIcon from '@coreui/icons-react';
 
@@ -29,12 +30,12 @@ export default () => {
   const [showModal, setShowModal] = useState(false);
   const [modalLoading, setModalLoading] = useState(false);
   const [modalTitleField, setModalTitleField] = useState('');
-  const [modalBodyField, setModalBodyField] = useState('');
+  const [modalFileField, setModalFileField] = useState('');
   const [modalId, setModalId] = useState('');
 
-  const fields= [
-    {label: 'Título', key: 'title'},
-    {label: 'Ações', key: 'actions', _style:{width:'1px'}}
+  const fields = [
+    { label: 'Título', key: 'title' },
+    { label: 'Ações', key: 'actions', _style: { width: '1px' } }
   ];
 
   useEffect(() => {
@@ -45,7 +46,7 @@ export default () => {
     setLoading(true);
     const result = await api.getDocuments();
     setLoading(false);
-    if(result.error === '') {
+    if (result.error === '') {
       setList(result.list);
     } else {
       alert(result.error);
@@ -59,14 +60,14 @@ export default () => {
   const handleEditButton = (index) => {
     setModalId(list[index]['id']);
     setModalTitleField(list[index]['title']);
-    setModalBodyField(list[index]['body']);
+    setModalFileField(list[index]['fileurl']);
     setShowModal(true);
   }
 
   const handleRemoveButton = async (index) => {
     if (window.confirm('Tem certeza que deseja excluir?')) {
-      const result = await api.removeWall(list[index]['id']);
-      if(result.error === '') {
+      const result = await api.removeDocument(list[index]['id']);
+      if (result.error === '') {
         getList();
       } else {
         alert(result.error);
@@ -74,44 +75,54 @@ export default () => {
     }
   }
 
-  
+
   const handleDownloadButton = async (index) => {
-   window.open(list[index]['fileurl']);
+    window.open(list[index]['fileurl']);
   }
 
   const handleNewButton = () => {
     setModalId('');
     setModalTitleField('');
-    setModalBodyField('');
+    setModalFileField('');
     setShowModal(true);
   }
 
   const handleModalSave = async () => {
 
-    if (modalTitleField && modalBodyField) {
-        setModalLoading(true);
-        let result;
+    if (modalTitleField) {
+      setModalLoading(true);
+      let result;
 
-        let data = {
-          title: modalTitleField,
-          body: modalBodyField
-        };
+      let data = {
+        title: modalTitleField
+      };
 
-        if (modalId === '') {
-          result = await api.addWall(data);
-        } else {
-          result = await api.updateWall(modalId, data);
+      if (modalId === '') {
+        if (modalFileField) {
+          data.file = modalFileField;
+          result = await api.addDocument(data);
         }
-
-        setModalLoading(false);
-        if(result.error === '') {
-            setShowModal(false);
-            getList();
-        } else {
-            alert(result.error);
+        else {
+          alert('Selecione o arquivo');
+          setModalLoading(false);
+          return;
         }
+      } else {
+        if (modalFileField) {
+          data.file = modalFileField;
+        }
+        result = await api.updateDocument(modalId, data);
+      }
+
+      setModalLoading(false);
+      if (result.error === '') {
+        setShowModal(false);
+        getList();
+      } else {
+        alert(result.error);
+      }
     } else {
-        alert('Preencha os campos!');
+      alert('Preencha os campos!');
     }
 
   }
@@ -121,7 +132,6 @@ export default () => {
       <CRow>
         <CCol>
           <h2>Documentos</h2>
-
           <CCard>
             <CCardHeader>
               <CButton color="primary" onClick={handleNewButton}>
@@ -143,7 +153,7 @@ export default () => {
                   'actions': (item, index) => (
                     <td>
                       <CButtonGroup>
-                      <CButton color='success' onClick={() => handleDownloadButton(index)}><CIcon name="cil-cloud-download" /></CButton>
+                        <CButton color='success' onClick={() => handleDownloadButton(index)}><CIcon name="cil-cloud-download" /></CButton>
                         <CButton color="info" onClick={() => handleEditButton(index)}>Editar</CButton>
                         <CButton color='danger' onClick={() => handleRemoveButton(index)}>Excluir</CButton>
                       </CButtonGroup>
@@ -159,43 +169,42 @@ export default () => {
 
 
       <CModal show={showModal} onClose={handleCloseModal}>
-        <CModalHeader closeButton>{modalId === '' ? 'Novo' : 'Editar'} Aviso</CModalHeader>
+        <CModalHeader closeButton>{modalId === '' ? 'Novo' : 'Editar'} Documento</CModalHeader>
         <CModalBody>
-            <CFormGroup>
-                <CLabel htmlFor="modal-title">Título do Aviso</CLabel>
-                <CInput
-                    type="text"
-                    id="modal-title"
-                    placeholder="Digite um título para o aviso"
-                    value={modalTitleField}
-                    onChange={e => setModalTitleField(e.target.value)}
-                    disabled={modalLoading}
-                />
-            </CFormGroup>
-            <CFormGroup>
-                <CLabel htmlFor="modal-body">Corpo do Aviso</CLabel>
-                <CTextarea
-                    id="modal-body"
-                    placeholder="Digite o conteúdo do aviso"
-                    value={modalBodyField}
-                    onChange={e => setModalBodyField(e.target.value)}
-                    disabled={modalLoading}
-                />
-            </CFormGroup>
+          <CFormGroup>
+            <CLabel htmlFor="modal-title">Título do Documento</CLabel>
+            <CInput
+              type="text"
+              id="modal-title"
+              placeholder="Digite um título do documento"
+              value={modalTitleField}
+              onChange={e => setModalTitleField(e.target.value)}
+              disabled={modalLoading}
+            />
+          </CFormGroup>
+          <CFormGroup>
+            <CLabel htmlFor="modal-file">Insira uma Imagem ou PDF</CLabel>
+            <CInput
+              type="file"
+              id="modal-file"
+              name="file"
+              onChange={e => setModalFileField(e.target.files[0])}
+            />
+          </CFormGroup>
         </CModalBody>
         <CModalFooter>
-            <CButton
-                color="primary"
-                onClick={handleModalSave}
-                disabled={modalLoading}
-            >
-              {modalLoading ? 'Carregando...' : 'Salvar'}
-            </CButton>
-            <CButton
-                color="secondary"
-                onClick={handleCloseModal}
-                disabled={modalLoading}
-            >Cancelar</CButton>
+          <CButton
+            color="primary"
+            onClick={handleModalSave}
+            disabled={modalLoading}
+          >
+            {modalLoading ? 'Carregando...' : 'Salvar'}
+          </CButton>
+          <CButton
+            color="secondary"
+            onClick={handleCloseModal}
+            disabled={modalLoading}
+          >Cancelar</CButton>
         </CModalFooter>
       </CModal>
     </>
