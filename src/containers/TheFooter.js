7ec1a432382af579ae3261e7883a77c65f3c57cb
adriano <adrianobr00@gmail.com>
2024-08-08 17:10:20 -0300

    atualizando folder

--- src/containers/TheFooter.js
+++ src/containers/TheFooter.js
@@ -5,8 +5,8 @@ const TheFooter = () => {
   return (
     <CFooter fixed={false}>
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">CoreUI</a>
-        <span className="ml-1">&copy; 2020 creativeLabs.</span>
+        <a href="https://agenciatecnet.com.br" target="_blank" rel="noopener noreferrer">Agência TecNet</a>
+        <span className="ml-1">&copy; 2024 .</span>
       </div>
       <div className="mfs-auto">
         <span className="mr-1">Powered by</span>
